deba3d3f15fa7a09251b4cd4772d202c6f7b3f61
Pantelis Papasavvas <pantpap@gmail.com>
2022-01-05 16:08:54 +0200

    create Card component and give it classes from other css module

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css'
 
 const AddUser = (props) => {
 
@@ -7,15 +9,17 @@ const AddUser = (props) => {
   }
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text"/>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"/>
 
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"/>
 
-      <button type="submit">Add User</button>
-    </form>
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   )
 }
 
